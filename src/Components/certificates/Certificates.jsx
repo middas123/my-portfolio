d3dfd122855259c2cc9bbcc005faa8e89b377ad3
Boitumelo Middas  Mokobane <125658597+middas123@users.noreply.github.com>
2023-08-21 15:39:18 +0200

    I worked on certificates.jsx and css
    I started working on the page because i was still on the about page and i am left to put css on it

--- src/Components/certificates/Certificates.jsx
+++ src/Components/certificates/Certificates.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React from 'react';
 import "./Certificates.css";
 
 const Certificates = () => {
   return (
-    <div>
-     
-        
+    <div className="certificates-wrapper">
+      <div className="certificates-heading">
+        <h2>Certificates</h2>
+        <div className="vertical-line"></div>
+      </div>
+      <div className="card-columns">
+        <div className="card">
+          <img src="..." className="card-img-top" alt="Certificate 1" />
+          <div className="card-body">
+            <h5 className="card-title">Certificate Title 1</h5>
+            <p className="card-text">Description of certificate 1.</p>
+          </div>
+        </div>
+        <div className="card p-3">
+          <blockquote className="blockquote mb-0 card-body">
+            <p>A well-known quote, contained in a blockquote element.</p>
+            <footer className="blockquote-footer">
+              <small className="text-muted">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </small>
+            </footer>
+          </blockquote>
+        </div>
+        <div className="card">
+          <img src="..." className="card-img-top" alt="Certificate 2" />
+          <div className="card-body">
+            <h5 className="card-title">Certificate Title 2</h5>
+            <p className="card-text">Description of certificate 2.</p>
+           
+          </div>
+        </div>
+        {/* More card components can be added here */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Certificates
+export default Certificates;
