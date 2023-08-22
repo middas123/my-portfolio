7c18edd5d5ea498a97e3636db3d18d8c2eb599f2
Boitumelo Middas  Mokobane <125658597+middas123@users.noreply.github.com>
2023-08-22 11:14:58 +0200

    certificates.css and jsx file
    I added a link to the code which so that the certificates can be downloaded and on the css i added a background color on the cards

--- src/Components/certificates/Certificates.jsx
+++ src/Components/certificates/Certificates.jsx
@@ -4,79 +4,67 @@ import "./Certificates.css";
 const Certificates = () => {
   return (
     <div className="certificates-wrapper" id='certificates'>
-          <div className="certificates-heading">
-            <h2>Certificates</h2>
-            <h />
-            <div className="vertical-line"></div>
+      <div className="certificates-heading">
+        <h2>Certificates</h2>
+        <h />
+        <div className="vertical-line"></div>
       </div>
       <div className="card-columns">
-          <div className="card">
-            
-            <div className="card p-2">
-              <br />
-              <h5 className="card-title">AWS Cloud Practitioner</h5>
-              <br />
-              <p className="card-text">It covers key concepts, benefits, and basic terms of AWS, Giving Indepth Understanding, It is designed for individuals who want to demonstrate their foundational knowledge and understanding of cloud computing concepts </p>
-              <button className="btn btn-primary">View Certificate</button>
-              <footer className="blockquote-footer">
-                
-              </footer>
-            </div>
+        <div className="card">
+          <div className="card p-2">
+            <br />
+            <h5 className="card-title">AWS Cloud Practitioner</h5>
+            <br />
+            <p>It covers key concept & basic terms of AWS, Giving In-depth Understanding, It is designed for individuals who want to demonstrate their skill</p>
+            <a href="src/assets/aws cloud prac.pdf" className="btn btn-primary" target="_blank" rel="noopener noreferrer">View Certificate</a>
+            <footer className="blockquote-footer">
+            </footer>
+          </div>
         </div>
         <div className="card">
-              <div className="card p-2">
-                <br />
-              <h5 className="card-title">AWS DevOps</h5>
-                <br />
-                  <p>covers a range of topics, including continuous integration, continuous delivery, automation,infrastructure, & continuous integration</p>
-                  <button className="btn btn-primary">View Certificate</button>
-                  <footer className="blockquote-footer">
-                    
-                  </footer>
-                
-              </div>
+          <div className="card p-2">
+            <br />
+            <h5 className="card-title">AWS DevOps</h5>
+            <br />
+            <p>Covers a range of topics, including continuous integration, continuous delivery, automation, infrastructure, & continuous integration</p>
+            <a href="src/assets/DevOpss.pdf" className="btn btn-primary" target="_blank" rel="noopener noreferrer">View Certificate</a>
+            <footer className="blockquote-footer">
+            </footer>
+          </div>
         </div>
         <div className="card">
-            <div className="card p-2">
-              <br />
+          <div className="card p-2">
+            <br />
             <h5 className="card-title">IBM Cyber Security</h5>
-              <br />
-                <p>It covers a wide range of topics related to protecting digital assets, networks & information systems from cyber threats and attacks.</p>
-                <button className="btn btn-primary">View Certificate</button>
-                <footer className="blockquote-footer">
-                  
-                </footer>
-            
-            </div>
+            <br />
+            <p>It covers a wide range of topics related to protecting digital assets, networks & information systems from cyber threats and attacks.</p>
+            <a href="src/assets/cybersecurity.pdf" className="btn btn-primary" target="_blank" rel="noopener noreferrer">View Certificate</a>
+            <footer className="blockquote-footer">
+            </footer>
+          </div>
         </div>
         <div className="card">
-         
-              <div className="card p-2">
-                <br />
-              <h5 className="card-title">AWS Technical Essentials</h5>
-              <br />
-                <p className="card-text"> It covers essential concepts, terminology, and key services offered by AWS.</p>
-                <button className="btn btn-primary">View Certificate</button>
-                <footer className="blockquote-footer">
-                  
-                </footer>
-              
-              </div>
+          <div className="card p-2">
+            <br />
+            <h5 className="card-title">AWS Technical Essentials</h5>
+            <br />
+            <p>It covers essential concepts, terminology, and key services offered by AWS on coursera</p>
+            <a href="src/assets/aws technical assen.pdf" className="btn btn-primary" target="_blank" rel="noopener noreferrer">View Certificate</a>
+            <footer className="blockquote-footer">
+            </footer>
+          </div>
         </div>
         <div className="card">
-        <div className="card p-2">
-          <br />
-              <h5 className="card-title">Figma Design</h5>
-                <br />
-                  <p>The Figma Design course provides instruction on how to effectively use Figma, a popular cloud-based design</p>
-                  <button className="btn btn-primary">View Certificate</button>
-                  <footer className="blockquote-footer">
-                    
-                  </footer>
-            
-              </div>
-              </div>
-      
+          <div className="card p-2">
+            <br />
+            <h5 className="card-title">Figma Design</h5>
+            <br />
+            <p>The Figma Design course provides instruction on how to effectively use Figma</p>
+            <a href="src/assets/Figma.pdf" className="btn btn-primary" target="_blank" rel="noopener noreferrer">View Certificate</a>
+            <footer className="blockquote-footer">
+            </footer>
+          </div>
+        </div>
       </div>
     </div>
   );
