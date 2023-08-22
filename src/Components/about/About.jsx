17e6f801a69451daed2872f5a88c8c6c7a88daab
Boitumelo Middas  Mokobane <125658597+middas123@users.noreply.github.com>
2023-08-22 14:49:10 +0200

    header.css
    I added a transform on the navbar

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -13,19 +13,18 @@ const About = () => {
             <Card.Img className="card-image" variant="top" src="src/assets/IMG_0330.jpeg" />
               <div style={{ flex: 3, padding: '0rem' }}>
                   <div className='card_content'>
-                  <Card.Body>
-                     Your content for the right side goes here 
-             
-                </Card.Body>
+                    
+      
                 </div>
             </div>
             </Card>
       <br />
       <div className='card_row'>
-        <Card style={{ width: '23rem' }}>
+        
+          <Card style={{ width: '23rem' }}>
        
-          <Card.Body>
-          <Card.Title style={{ color: '#333403', fontWeight: 'bold', fontSize: '1.5rem' }}>Skills</Card.Title>
+           <Card.Body>
+           <Card.Title style={{ color: '#333403', fontWeight: 'bold', fontSize: '1.5rem' }}>Skills</Card.Title>
                     <Card.Img variant="top" src="src/assets/icons8-html-48.png" alt="Card Image" />
                     <Card.Img variant="top" src="src/assets/icons8-css-48.png" alt="Card Image" />
                     <Card.Img variant="top" src="src/assets/icons8-javascript-64.png" alt="Card Image" />
@@ -34,7 +33,7 @@ const About = () => {
                     <Card.Img variant="top" src="src/assets/icons8-java-48.png" alt="Card Image" />
                     <Card.Img variant="top" src="src/assets/icons8-python-48.png" alt="Card Image" />
                     <Card.Img variant="top" src="src/assets/icons8-figma-100.png" alt="Card Image" />
-
+        
           </Card.Body>
         </Card>
 
