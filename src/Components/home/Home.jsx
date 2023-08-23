b48809a6a8242e3abd7b0626273583910771fa8d
Boitumelo Middas  Mokobane <125658597+middas123@users.noreply.github.com>
2023-08-23 13:57:07 +0200

    home
    Home page was just too clumsy , so i divided the divs that i had and i decreased the margin-top of the text container

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import Typed from 'typed.js';
-import { FaLinkedin, FaTwitter, FaGithub, FaInstagram } from 'react-icons/fa'; // Import icons from react-icons
+import { FaLinkedin, FaTwitter, FaGithub } from 'react-icons/fa'; // Import icons from react-icons
 import 'animate.css';
 import './home.css';
 
 const Home = () => {
-  const profileImageUrl = 'src/assets/about_pic-removebg-preview (1).png';
+  const profileImageUrl = 'src/assets/IMG_0330.jpeg';
 
   useEffect(() => {
     const options = {
@@ -51,9 +51,7 @@ const Home = () => {
                 <a href="YOUR_GITHUB_URL" target="_blank" rel="noopener noreferrer">
                   <FaGithub />
                 </a>
-                <a href="YOUR_GITHUB_URL" target="_blank" rel="noopener noreferrer">
-                  <FaInstagram />
-                </a>
+                
               </span>
             </p>
           </div>
