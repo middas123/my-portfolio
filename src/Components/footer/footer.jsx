d076fb63f8c47368d541e7f407c5c2d3cd07fa12
Boitumelo Middas  Mokobane <125658597+middas123@users.noreply.github.com>
2023-08-24 11:47:29 +0200

    footer
    I added a footer folder and its files

---
+++ src/Components/footer/footer.jsx
@@ -0,0 +1,17 @@
+<div className='socials'>
+<span className="social-icons">
+  {/* Add social media icons with custom rotation animation */}
+  <a href="https://www.linkedin.com/in/boitumelo-middas-mokobane-544280148/" className="rotate-icon" target="_blank" rel="noopener noreferrer">
+    <FaLinkedin />
+  </a>
+  <a href="https://twitter.com/MiddasMoko44348" className="rotate-icon" target="_blank" rel="noopener noreferrer">
+    <FaTwitter />
+  </a>
+  <a href="https://github.com/middas123" className="rotate-icon" target="_blank" rel="noopener noreferrer">
+    <FaGithub />
+  </a>
+  <a href="https://www.instagram.com/middasmoks/" className="rotate-icon" target="_blank" rel="noopener noreferrer">
+    <FaInstagram />
+  </a>
+</span>
+</div>
