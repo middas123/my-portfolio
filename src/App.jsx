6733ef968015df4bbc14fc870b64a916e77587c9
Boitumelo Middas  Mokobane <125658597+middas123@users.noreply.github.com>
2023-08-25 13:07:56 +0200

    app.jsx
    I removed some unimportant functions, because they were giving me an error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Header from './Components/header/Header'
@@ -9,7 +9,7 @@ import Certificates from './Components/certificates/Certificates'
 import Contact from './Components/contact/Contact'
 import Footer from './Components/footer/footer';
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div>
