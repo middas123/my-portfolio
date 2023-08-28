1c84bc19f6c2ee966b4a611bb0233f489f820a96
Boitumelo Middas  Mokobane <125658597+middas123@users.noreply.github.com>
2023-08-28 16:01:23 +0200

    Firebase
    I added a file called firebase.jsx

---
+++ src/firebase.jsx
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCk0dyrNHhxZGWARpB1TdoQhxHAwQXBtDU",
+  authDomain: "vivid-now-332706.firebaseapp.com",
+  projectId: "vivid-now-332706",
+  storageBucket: "vivid-now-332706.appspot.com",
+  messagingSenderId: "752097036479",
+  appId: "1:752097036479:web:0ef4dd016eea84dbdade0a",
+  measurementId: "G-JFG9HGHXMK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
