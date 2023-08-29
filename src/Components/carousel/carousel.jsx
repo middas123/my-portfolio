61345971339bfb3eaeb8faf3fe4c408707524416
Boitumelo Middas  Mokobane <125658597+middas123@users.noreply.github.com>
2023-08-29 13:26:42 +0200

    Carousel
    I added a page which is called Carousel, i am planning to replace the page Services with a carousel

--- src/Components/carousel/carousel.jsx
+++ src/Components/carousel/carousel.jsx
@@ -1,46 +1,50 @@
+import React from 'react';
+import './carousel.css';
 
-
-const CarouselComponent = () => {
+function Carousel() {
   return (
-    <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
-      <ol className="carousel-indicators">
-        <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
-        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-      </ol>
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src="https://via.placeholder.com/800x400" className="d-block w-100" alt="Slide 1" />
-          <div className="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
+    <div>
+      <h2>Featured Images</h2>
+      <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="..." className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>Some representative placeholder content for the third slide.</p>
+            </div>
           </div>
         </div>
-        <div className="carousel-item">
-          <img src="https://via.placeholder.com/800x400" className="d-block w-100" alt="Slide 2" />
-          <div className="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>Some representative placeholder content for the second slide.</p>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <img src="https://via.placeholder.com/800x400" className="d-block w-100" alt="Slide 3" />
-          <div className="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-          </div>
-        </div>
+        <button className="carousel-control-prev" type="button" data-target="#carouselExampleCaptions" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-target="#carouselExampleCaptions" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </button>
       </div>
-      <button className="carousel-control-prev" type="button" data-target="#carouselExampleCaptions" data-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="sr-only">Previous</span>
-      </button>
-      <button className="carousel-control-next" type="button" data-target="#carouselExampleCaptions" data-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="sr-only">Next</span>
-      </button>
     </div>
   );
-};
+}
 
-export default CarouselComponent;
+export default Carousel;
